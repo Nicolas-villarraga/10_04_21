ea9d97066a31e08b345df5787731de1bebfc9de2
Nicolas-villarraga <80805713+Nicolas-villarraga@users.noreply.github.com>
2021-04-10 22:11:21 -0500

    Initial commit

---
+++ database/migrations/2021_04_11_014647_create_product_stores_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_stores', function (Blueprint $table) {
+            $table->id();
+            $table->bigint('amount');
+            $table->string('products_storecol',45);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_stores');
+    }
+}
